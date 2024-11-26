826bd0dc3efd4ea8c4a4075058a7fd0200f4084b
whitehairdevjs <jsuserwork20@gmail.com>
2024-11-26 15:12:57 +0900

    Swagger 설정 및 webconfig 셋팅.

---
+++ src/main/java/com/example/prac/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.prac.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대하여
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
--- src/main/java/com/example/prac/controller/UserController.java
+++ src/main/java/com/example/prac/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.prac.controller;
 
 import com.example.prac.entity.User;
 import com.example.prac.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,16 +16,19 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "전체 유저 정보.", description = "전체 유저 정보 가져오기.")
     @GetMapping
     public List<User> getAllUsers() {
         return userService.findAll();
     }
 
+    @Operation(summary = "id의 유저 정보.", description = "id로 유저 정보 가져오기.")
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
         return userService.findById(id);
     }
 
+    @Operation(summary = "유저 정보 저장.", description = "유저 정보 저장하기.")
     @PostMapping
     public void createUser(@RequestBody User user) {
         userService.save(user);
